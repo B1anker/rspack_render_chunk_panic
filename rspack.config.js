a9950c54ef85b9c7c91d8ab3a72acfd95db1f01f
seaveyluo <seaveyluo@tencent.com>
2023-04-08 18:21:49 +0800

    chore: init

---
+++ rspack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/index.jsx",
+  },
+  target: "es5",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  builtins: {
+    html: [{ template: "./src/index.html" }],
+  },
+};
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const App = () => {
+  return <div>hello world</div>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
